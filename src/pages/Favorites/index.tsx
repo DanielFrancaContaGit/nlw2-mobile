a067b57add4db816984be4f5bf34eab74c4e81f8
Daniel França <daniel.pe.15y15@gmail.com>
2020-08-11 14:38:41 -0300

    feat/fifth

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,49 @@
-import  React from 'react';
-import { View } from 'react-native';
+import  React, { useState, useEffect } from 'react';
+import { View, ScrollView, Text } from 'react-native';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import styles from './style';
 
-function Favorites() {
+function Favorites() {  
+  const [favorites, setFavorites] = useState([]);
+
+  async function loadingFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if(response) {
+        const favoritedTeachers = JSON.parse(response);
+        setFavorites(favoritedTeachers);
+      };
+    });
+  }
+
+  useFocusEffect(() => {
+    loadingFavorites();
+  });
+  
   return(
     <View style={styles.container}>
       <PageHeader title='Meus proffys favoritos'/>
+
+      <ScrollView 
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        { favorites.map((teacher: Teacher ) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited
+            />
+          )
+        })}
+      </ScrollView>
     </View>
   )
 }
